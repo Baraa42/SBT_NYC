037add9f37ccf04878d8ef8cfb0e68bc92f612e3
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-06-25 17:23:07 -0400

    guild script

--- Contracts/contracts/TestOracle.sol
+++ Contracts/contracts/TestOracle.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "./interfaces/IOracle.sol";
 
 contract TestOracle is IOracle {
-    function hasCompleted(address from) external view returns (bool) {
+    function hasCompleted(address from) external view override returns (bool) {
         return true;
     }
 }
---
+++ Contracts/scripts/createGuilds.js
@@ -0,0 +1,72 @@
+const { ethers, network } = require("hardhat")
+
+async function mint() {
+    const guild = await ethers.getContract("Guild")
+    const oracle = await ethers.getContract("TestOracle")
+    const oracleAddress = oracle.address
+
+    console.log("Creating guild...")
+    accounts = await ethers.getSigners() // could also do with getNamedAccounts
+    deployer = (await getNamedAccounts()).deployer
+    user = accounts[1]
+    const firstGuildDetails = {
+        description: "This is my First guild",
+        uri: "ipfs://QmPKDSGxdGaKEE2xF9MvQUkGeYqNJH6S8kXfu5fDBtVWW9",
+        oracleContract: oracleAddress,
+        rewardToken: "0x0000000000000000000000000000000000000000",
+        rewardAmount: "0",
+        totalRewardAmount: "0",
+    }
+    const createTx = await guild.createGuild("Guild", "GLD", firstGuildDetails)
+    const createRx = await createTx.wait(1)
+    console.log("First Guild created")
+    const secondGuildDetails = {
+        description: "This is my second guild",
+        uri: "ipfs://QmPKDSGxdGaKEE2xF9MvQUkGeYqNJH6S8kXfu5fDBtVWW9",
+        oracleContract: oracleAddress,
+        rewardToken: "0x0000000000000000000000000000000000000000",
+        rewardAmount: "0",
+        totalRewardAmount: "0",
+    }
+    const createTx2 = await guild.createGuild(
+        "Guild",
+        "GLD",
+        secondGuildDetails
+    )
+    const createRx2 = await createTx2.wait(1)
+    console.log("Second Guild created")
+    const thirdGuildDetails = {
+        description: "This is my third guild",
+        uri: "ipfs://QmPKDSGxdGaKEE2xF9MvQUkGeYqNJH6S8kXfu5fDBtVWW9",
+        oracleContract: oracleAddress,
+        rewardToken: "0x0000000000000000000000000000000000000000",
+        rewardAmount: "0",
+        totalRewardAmount: "0",
+    }
+    const createTx3 = await guild.createGuild(
+        "Guild",
+        "GLD",
+        secondGuildDetails
+    )
+    const createRx3 = await createTx3.wait(1)
+    console.log("Third Guild created")
+    const guildCounter = await guild.guildCounter()
+    console.log("guildCounter:", guildCounter.toString())
+
+    // const transferTx = await soulbound
+    //     .connect(user)
+    //     .transferFrom(
+    //         user.address,
+    //         "0x6CAc6323c14C14c71bF5babd1328279153e049a6",
+    //         0
+    //     )
+    // const transferRx = await transferTx.wait(1)
+    // console.log(transferRx)
+}
+
+mint()
+    .then(() => process.exit(0))
+    .catch((e) => {
+        console.log(e)
+        process.exit(1)
+    })
